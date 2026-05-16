1d2e7a80ffe823a859ef030b51655ab10ec207f8
Emiliano Nicolas Mendoza Garay <mendozanikolas000@gmail.com>
2026-05-16 12:30:45 -0600

    updated: app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RutasModule } from './rutas/rutas.module';
 import { CiudadesModule } from './ciudades/ciudades.module';
 import { TicketsModule } from './tickets/tickets.module';
@@ -9,15 +9,19 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
       type: 'postgres',
-      host: 'localhost',
-      port: 5435,
-      username: 'admin_taller',
-      password: 'password123',
-      database: 'bd_tickets_final',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USER'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_NAME'),     
       autoLoadEntities: true,
       synchronize: true,
+      }),
     }),
     RutasModule,
     CiudadesModule,
